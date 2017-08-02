06a7f08c65d25a3daef50a928fa301898387a822
Matias Verardo <verardomatias@gmail.com>
2017-08-02 15:50:16 -0700

    test

--- codigo/js/index.js
+++ codigo/js/index.js
@@ -1,16 +1,45 @@
 $( document ).ready(function() {
-    $('.carousel1').slick({
-    	slidesToShow: 3,
-    	slidesToScroll: 1,
-    	asNavFor: '.slider-for',
-    	dots: true,
-    	focusOnSelect: true
-    });
+	// $('.carousel1').slick({
+	// 	slidesToShow: 3,
+	// 	slidesToScroll: 1,
+	// 	asNavFor: '.slider-for',
+	// 	dots: true,
+	// 	focusOnSelect: true
+	// });
 
-    $('.carousel2').slick({
-    	infinite: false,
-    	slidesToShow: 3,
-    	slidesToScroll: 3
-    });
+	// $('.carousel2').slick({
+	// 	infinite: false,
+	// 	slidesToShow: 3,
+	// 	slidesToScroll: 3
+	// });
+	$(".carousel1").slick({
+		autoplay:true,
+		autoplaySpeed:10000,
+		speed:600,
+		slidesToShow:1,
+		slidesToScroll:1,
+		pauseOnHover:false,
+		dots:true,
+		pauseOnDotsHover:true,
+		cssEase:'linear',
+		draggable:false,
+		prevArrow:'<button class="PrevArrow"></button>',
+		nextArrow:'<button class="NextArrow"></button>'
+	});
+
+	$(".carousel2").slick({
+		autoplay:true,
+		autoplaySpeed:10000,
+		speed:600,
+		slidesToShow:1,
+		slidesToScroll:1,
+		pauseOnHover:false,
+		dots:true,
+		pauseOnDotsHover:true,
+		cssEase:'linear',
+		draggable:false,
+		prevArrow:'<button class="PrevArrow"></button>',
+		nextArrow:'<button class="NextArrow"></button>'
+	});
 });
 
